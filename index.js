015ce53d598e64d50b8661fb66331c52aa0221ab
Sandeep Sharma <sandeepsharma@naraci.com>
2024-11-13 10:01:48 +0530

    login and register functionality done

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRouter = require("./routes/userRoute");
+const port = process.env.PORT || 5003;
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  //   res.send("Home Page");
+  res.status(200).json({
+    status: "success",
+    message: "Home",
+  });
+});
+
+app.use("/user", userRouter);
+
+mongoose.set("strictQuery", true);
+
+app.listen(port, () => {
+    console.log('Server started at port ' + port);
+});
