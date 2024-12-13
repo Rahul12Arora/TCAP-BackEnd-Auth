5feefbf4df2922d0c61b62b041cce56d6879d10d
Rahul Arora <rahul.012.arora@gmail.com>
2024-12-13 20:33:03 +0530

    new password

--- startup/dbConnection.js
+++ startup/dbConnection.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const uri = "mongodb+srv://rahul012arora:thzCDyr1ihlKcejq@tcap.cgyuo.mongodb.net/?retryWrites=true&w=majority&appName=TCAP";
+const uri = "mongodb+srv://rahul012arora:tAL43E364xYa9Wbw@tcap.cgyuo.mongodb.net/?retryWrites=true&w=majority&appName=TCAP";
 const client = new MongoClient(uri);
 let isConnected = false;
 
