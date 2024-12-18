d7259214a26d8132cfe941c74fbf436a122a17bc
Sandeep Sharma <sandeepsharma@naraci.com>
2024-12-18 15:27:29 +0530

    room Fix

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const UserRouter = require("./routes/userRoute");
 const ChatGroupRouter = require("./routes/ChatGroupRoutes");
-const port = process.env.PORT || 5003;
+const port = process.env.PORT || 8080;
 const getDb = require('./startup/dbConnection')
 const listCollections = require('./startup/listCollections')
 const startSocketConnection = require("./startup/socketIO");
 const { Server } = require('socket.io');
 const http = require('http');
 dotenv.config();
+console.log("port -> ", port);
 
 const app = express();
 app.use(express.json());
@@ -21,21 +22,55 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "http://localhost:3000", // Dynamic frontend URL
+    origin: process.env.FRONTEND_URL || "http://localhost:3001", // Dynamic frontend URL
     methods: ["GET", "POST"],
   },
 });
 
+// Store user info (assuming you are storing usernames with socket.id)
+const userMap = {};
+const callBack = (msg) => {
+  console.log("Call Back -> ", msg);
+}
+
 // Handle socket connections
 io.on('connection', (socket) => {
   // console.log('A user connected:', socket.id);
+  // Join a room
+  socket.on('JoinRoom', (payload) => {
+    console.log("Payload room -> ", payload);
+    socket.join(payload.roomId);
+    socket.leave(payload.oldRoomId);
+    console.log(`User ${payload.userName} joined room: ${payload.roomName}`);
+    userMap[socket.id] = payload.userName;
+
+    // Notify others in the room
+    // socket.to(room).emit('message', `${socket.id} has joined the room.`);
+    socket.to(payload.roomId).emit('RoomJoined', { payload });
+  });
 
   // Example of listening to an event
   socket.on('message', (data) => {
-    // console.log('Message received from frontend -> ', data);
+    console.log('Message received from frontend -> ', data.room, data.payload.msgBody, data.payload.userDetails.name);
+
     // data += " This is added from backend";
     // Broadcast the message to all connected clients
-    io.emit('message', data);
+    // socket.to(data.room).emit('message', data.payload);
+    // socket.to(data.room).emit('message', data.payload, (response) => {
+    //   // Callback function to confirm delivery
+    //   console.log("Message delivered to clients:", response);
+    //   // callback('Message delivered successfully');
+    // });
+    if (io.sockets.adapter.rooms.get(data.room)) {
+      io.to(data.room).emit('message', data.payload, () => {
+        console.log("Message delivered to clients in room:", data.room);
+        // callBack('Message delivered successfully');
+      });
+    } else {
+      console.log("Room does not exist:", data.room);
+      callBack('Room does not exist');
+    }
+    // io.emit('message', data);
   });
 
   // Handle disconnection
