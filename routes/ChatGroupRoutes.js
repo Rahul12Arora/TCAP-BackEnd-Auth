531b66e19d6ded31e5dfd3b8ae0ed8577a8352a8
Sandeep Sharma <sandeepsharma@naraci.com>
2024-12-19 01:44:28 +0530

    adding new routes

--- routes/ChatGroupRoutes.js
+++ routes/ChatGroupRoutes.js
@@ -25,6 +25,28 @@ router.post("/createNewChatGroup", async (req, res) => {
     }
 });
 
+
+router.post("/add-new-member-to-group", async (req, res) => {
+    try {
+        const { groupId, users } = req.body;
+        const chatGroup = await ChatGroup.findByIdAndUpdate(
+            groupId,
+            { $push: { users: { $each: users } } }, // Correctly structure $push with $each
+            { new: true } // Optionally return the updated document
+          );
+        for (let i = 0; i < users.length; i++) {
+            await User.findByIdAndUpdate(users[i], {
+                $push: { chatGroups: chatGroup._id },
+            });
+        }
+
+        res.status(200).json(chatGroup);
+    } catch (error) {
+        console.error("Error -> ", error);
+        res.status(400).json(error);
+    }
+});
+
 router.get("/getAllChatGroup", async (req, res) => {
     try {
         const chatGroup = await ChatGroup.find().populate("createdBy users");
