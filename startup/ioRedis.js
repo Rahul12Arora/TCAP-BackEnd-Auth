be36e551f1e97a19e387d98841fd75e1ec7c67a2
Rahul Arora <rahul.012.arora@gmail.com>
2025-01-19 23:28:00 +0530

    Update ioRedis.js

--- startup/ioRedis.js
+++ startup/ioRedis.js
@@ -3,6 +3,13 @@ const Redis = require("ioredis");
 // Create a Redis instance.
 // By default, it will connect to localhost:6379.
 // We are going to cover how to specify connection options soon.
-const redis = new Redis();
+
+// const redis = new Redis(); // original 
+
+// new updation for aws
+const redis = new Redis({
+  host: '127.0.0.1',  // Explicitly specify the host
+  port: 6379,         // Explicitly specify the port
+});
 
 module.exports = redis;
