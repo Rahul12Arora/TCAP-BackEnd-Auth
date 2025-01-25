3643e59c72b3423230d1c18afc3fdf0781df9cef
Sandeep Sharma <sandeepsharma@naraci.com>
2025-01-25 15:20:58 +0530

    Group Joining alteration

--- socketSepration/groupChatSocket.js
+++ socketSepration/groupChatSocket.js
@@ -9,12 +9,14 @@ function groupSocketHandler (socket, io) {
     // Commenting this line so that when user logins he can connect to all groups
     // socket.leave(payload.oldRoomId);
     console.log(`User ${payload.userName} joined room: ${payload.roomName}`);
+    payload.msgBody = `${payload.userName} joined: ${payload.roomName}`;
+    payload.userDetails = {name: payload.userName};
     // userMap[socket.id] = payload.userName;
 
     // Notify others in the room
     // socket.to(room).emit('message', `${socket.id} has joined the room.`);
     io.of('/groupChat').to(payload.roomId).emit('RoomJoined', { payload });
-    io.of('/groupChat').to(payload.roomId).emit('message', { payload });
+    io.of('/groupChat').to(payload.roomId).emit('message', { ...payload });
     // io.of('/groupChat').to(payload.roomId).emit('message', 'hello from grpupchat namespace room');
     // io.to(payload.roomId).emit('message', { payload });
     // socket.emit('message', { payload });
